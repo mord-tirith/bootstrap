18ac717a4866d68f4bcca9a6a7da164445e283d1
mord-tirith <thiagohfreitasn@gmail.com>
2026-04-09 23:03:19 +0100

    added tree bat and fd install script

---
+++ scripts/packages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+CURLER="$SCRIPT_DIR"
+CURLER+="/curler.sh"
+
+"$CURLER" https://gitlab.com/OldManProgrammer/unix-tree/-/archive/2.3.2/unix-tree-2.3.2.tar.bz2 tree -f
+
+"$CURLER" https://github.com/sharkdp/fd/releases/download/v10.4.2/fd-v10.4.2-x86_64-unknown-linux-gnu.tar.gz fd -f
+
+"$CURLER" https://github.com/sharkdp/bat/releases/download/v0.26.1/bat-v0.26.1-x86_64-unknown-linux-musl.tar.gz bat -f 
+
